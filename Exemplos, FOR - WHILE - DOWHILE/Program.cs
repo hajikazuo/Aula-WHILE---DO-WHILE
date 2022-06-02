9a244eb5665c979c4ee0b723c75370d372cac80a
curso.ads <curso.ads@LAB06-68>
2022-06-01 21:37:10 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplos, FOR - WHILE - DOWHILE/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplos__FOR___WHILE___DOWHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo de estrutura WHILE
+            int a = 0;
+
+            Console.WriteLine("Antes\n");
+
+            while (a < 0)
+            {
+                a++;
+                Console.WriteLine("Durante\n");
+            }
+            Console.WriteLine("Depois\n");
+
+            Console.WriteLine("---------------------------");
+
+          
+            //Exemplo de estrutura DO WHILE
+            int b = 0;
+
+            Console.WriteLine("Antes\n");
+
+            do
+            {
+                b++;
+                Console.WriteLine("Durante\n");
+            }while (b < 0);
+
+            Console.WriteLine("Depois\n");
+            Console.ReadKey();
+        }
+    }
+}
